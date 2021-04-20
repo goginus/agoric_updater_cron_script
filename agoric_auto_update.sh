cebbb7c5ea565a704920a473df3bd602ac2149ce
Kazantsev.Andrey <Kazantsev.Andrey@russianpost.ru>
2021-04-20 19:21:56 +0500

    Create agoric_auto_update.sh

---
+++ agoric_auto_update.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+currentChainVersion=$(curl 'http://localhost:26657/status?' | jq '.result.node_info.network')
+networkChainVersion=$(curl 'https://testnet.agoric.net/network-config' | jq '.chainName')
+
+
+#Difference check chainName
+if [ "$currentChainVersion" != "$networkChainVersion" ]
+then
+service ag-chain-cosmos stop
+cd $HOME
+rm -rf agoric-sdk
+git clone https://github.com/Agoric/agoric-sdk -b $networkChainVersion
+cd agoric-sdk
+yarn install
+yarn build
+(cd packages/cosmic-swingset && make)
+curl https://testnet.agoric.net/network-config > chain.json
+chainName=`jq -r .chainName < chain.json`
+curl https://testnet.agoric.net/genesis.json > $HOME/.ag-chain-cosmos/config/genesis.json 
+ag-chain-cosmos unsafe-reset-all
+peers=$(jq '.peers | join(",")' < chain.json)
+seeds=$(jq '.seeds | join(",")' < chain.json)
+sed -i.bak 's/^log_level/# log_level/' $HOME/.ag-chain-cosmos/config/config.toml
+sed -i.bak -e "s/^seeds *=.*/seeds = $seeds/; s/^persistent_peers *=.*/persistent_peers = $peers/" $HOME/.ag-chain-cosmos/config/config.toml
+service ag-chain-cosmos start
+else
+fi
+
